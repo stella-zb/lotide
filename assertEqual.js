721fd84b8ab5db41bf64235267d6628a5858d45b
stella-zb <upperdaystudio@gmail.com>
2019-10-15 01:29:05 +0000

    Implement assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+// Function Implementation
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected) : console.log("🔴🔴🔴 Assertion Failed: " + actual + " !== " + expected);
+};
+
+//Test Code
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 2);
