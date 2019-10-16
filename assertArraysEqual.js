65ef83872c4baf53671c30ec2d51979083001b5a
stella-zb <upperdaystudio@gmail.com>
2019-10-16 13:18:51 +0000

    Implement assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,23 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) return false;
+    }
+    return true;
+  }
+};
+
+// Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.
+const assertArraysEqual = function(actual, expected) {
+  const result = eqArrays(actual, expected);
+  if (result === true) {
+    console.log(`✅✅✅ Assertion passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion failed: [${actual}] === [${expected}]`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
