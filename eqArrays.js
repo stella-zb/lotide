e3d41228135580234365786d4496ef3030dfaf8f
stella-zb <upperdaystudio@gmail.com>
2019-10-21 23:16:57 +0000

    refactor the code with test file

--- eqArrays.js
+++ eqArrays.js
@@ -1,7 +1,4 @@
-const assertEqual = function(actual, expected) {
-  //actual === expected ? console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected) : console.log("🔴🔴🔴 Assertion Failed: " + actual + " !== " + expected);
-  actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-};
+const assertEqual = require('./assertEqual');
 // Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match.
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
@@ -14,8 +11,4 @@ const eqArrays = function(arr1, arr2) {
   }
 };
 
-// Test cases for various scenarios
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+module.exports = eqArrays;
