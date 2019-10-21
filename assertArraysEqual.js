94e1f0b65179a0b4b9472c571be26ffe55146d23
stella-zb <upperdaystudio@gmail.com>
2019-10-21 23:24:35 +0000

    refactor the code with test file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -20,5 +20,4 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
+module.exports = assertArraysEqual;
