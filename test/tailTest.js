ab220839b60812be303c6bbfc65c5264ca040384
stella-zb <upperdaystudio@gmail.com>
2019-10-22 00:42:52 +0000

    convert with Mocha and Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,13 @@
-const assertEqual = require('../assertEqual');
+// const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words);
+// assertEqual(words.length, 3);
+
+describe('#tail', () => {
+  it('return 3 for ["Yo Yo", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]).length, 3);
+  });
+});
